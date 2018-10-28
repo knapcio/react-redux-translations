2e5eeb8494daa9b97475c852f313a9ec7c3e0f05
zbyszek <knapcio@gmail.com>
2018-10-28 17:14:29 +1000

    commented our load action

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,11 @@
 export const types = {
-    LOAD_TRANSLATIONS: 'LOAD_TRANSLATIONS',
+    // LOAD_TRANSLATIONS: 'LOAD_TRANSLATIONS',
     SET_TRANSLATIONS: 'SET_TRANSLATIONS'
 }
 
 export const actions = 
 {
-    loadTranslations: () => ({ type: types.LOAD_TRANSLATIONS }),
+    // loadTranslations: () => ({ type: types.LOAD_TRANSLATIONS }),
     setTranslations: translations => ({ type: types.SET_TRANSLATIONS, translations })
 }
 
